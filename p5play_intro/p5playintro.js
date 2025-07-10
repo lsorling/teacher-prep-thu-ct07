e15ee2b43d01c7696524f5cd6c1c4a6a6d8b78ab
lsorling <lsorling@yahoo.com.sg>
2025-07-10 03:59:13 +0000

    Jul 10, 2025, 3:59 AM

--- p5play_intro/p5playintro.js
+++ p5play_intro/p5playintro.js
@@ -7,7 +7,9 @@ function setup() {
   // write your codes here
   fill("skyblue");
   stroke("pink");
-  strokeW
+  strokeWeight(10);
+
+  circle()
   // End Basic shape testing
 
    // Create a bouncing ball sprite
