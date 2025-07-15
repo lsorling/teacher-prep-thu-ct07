ad71047a92cde8ad6c323f9d3ba2894c61d5c4ef
lsorling <lsorling@yahoo.com.sg>
2025-07-15 04:09:34 +0000

    Jul 15, 2025, 4:09 AM

--- flappybird/playground/playground.js
+++ flappybird/playground/playground.js
@@ -90,7 +90,8 @@ function draw() {
     image(bg, 0, 0, width, height);
 
     if (kb.presses('space') || mouse.presses()) {
-        startScreenLabel.v
+        startScreenLabel.visible = false;
+        bird.v
         startGame = true;
     }
 
