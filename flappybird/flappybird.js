11db124b081b4f0aaa841d691d4a3e1003508443
lsorling <lsorling@yahoo.com.sg>
2025-07-15 02:48:42 +0000

    Lesson 4

--- flappybird/flappybird.js
+++ flappybird/flappybird.js
@@ -1,1 +1,14 @@
 // the final game
+
+// run once like the "when green flag clicked"
+function setup() {
+    new Canvas(400,600);
+    background("steelblue");
+}
+
+// forever block
+// must have this empty function if you have any new Sprite() code 
+// in the setup()
+function draw() {
+
+}
