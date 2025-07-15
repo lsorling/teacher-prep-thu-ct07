7b26e19163521845eeca622d7ed7a3b1ca48212e
lsorling <lsorling@yahoo.com.sg>
2025-07-15 04:22:56 +0000

    Jul 15, 2025, 4:22 AM

--- flappybird/playground/playground.js
+++ flappybird/playground/playground.js
@@ -99,7 +99,7 @@ function draw() {
 
         // 4.4 keyboard and mouse inputs
         if (kb.presses('space') || mouse.presses()) {
-            bird.vel.y -= 1;
+            bird.vel.y = -0.5;
             bird.sleeping = false; // wake up if fallen asleep
         }
 
