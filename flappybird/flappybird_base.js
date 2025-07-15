9472f2f11a19dbc08e826de1fa02cbd31a37a9bd
lsorling <lsorling@yahoo.com.sg>
2025-07-15 02:54:50 +0000

    Jul 15, 2025, 2:54 AM

---
+++ flappybird/flappybird_base.js
@@ -0,0 +1,36 @@
+// 
+
+// the global variables section
+let bird, floor;
+let flapMidImg, bg, base;
+
+// preload game assets like media and images
+function preload() {
+    // bird image, background image, and the floor
+    flapMidImg = loadImage('assets/yellowbird-midflap.png');
+    bg = loadImage('assets/background-day.png');
+    base = loadImage('assets/base.png');
+}
+
+// run once like the "when green flag clicked"
+function setup() {
+    new Canvas(400,600);
+
+    // bird sprite
+    bird = new Sprite();
+    bird.x = width / 2;
+    bird.y = 200;
+    bird.width = 30;
+    bird.height = 30;
+    bird.img = flapMidImg;
+
+    // background("steelblue");
+}
+
+// forever block
+// must have this empty function if you have any new Sprite() code 
+// in the setup()
+function draw() {
+    // draw the background image
+    image(bg, 0, 0, width, height);
+}
