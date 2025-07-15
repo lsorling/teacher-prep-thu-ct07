760dea12deb1a87c2b16afd609cf6afc343b86c4
lsorling <lsorling@yahoo.com.sg>
2025-07-15 03:49:33 +0000

    Jul 15, 2025, 3:49 AM

--- flappybird/playground/playground.js
+++ flappybird/playground/playground.js
@@ -132,11 +132,11 @@ function spawnPipePair() {
     let midY = height / 2;
 
     // create the bottom pipe sprite
-    bottomPipe = new Sprite(400, midY + gap/2 +200, 52, 320, 'static');
+    bottomPipe = new Sprite(bird.x + 400, midY + gap/2 +200, 52, 320, 'static');
     bottomPipe.img = pipe;
 
     // now the top pipe sprite
-    topPipe = new Sprite(400, midY - gap/2 -200, 52, 320, 'static');
+    topPipe = new Sprite(bird.x + 400, midY - gap/2 -200, 52, 320, 'static');
     topPipe.img = pipe;
     topPipe.rotation = 180; // upside down
 
