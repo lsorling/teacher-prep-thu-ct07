0ee14de03e0d48d28ab03cc0faf8bfec6481c864
lsorling <lsorling@yahoo.com.sg>
2025-07-15 03:05:01 +0000

    Jul 15, 2025, 3:05 AM

--- flappybird/flappybird.js
+++ flappybird/flappybird.js
@@ -65,5 +65,6 @@ function draw() {
     // 4.5 debug info on screen
     fill("blue");
     textSize(14);
-    text('vel.y: ' + bird.vel.y.toFixed(2), 10, 20)
+    text('vel.y: ' + bird.vel.y.toFixed(2), 10, 20);
+    
 }
