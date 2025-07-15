23411c89081218b00d036250d5358268b6afbe66
lsorling <lsorling@yahoo.com.sg>
2025-07-15 03:36:07 +0000

    Jul 15, 2025, 3:36 AM

--- flappybird/playground/playground.js
+++ flappybird/playground/playground.js
@@ -121,6 +121,7 @@ function spawnPipePair() {
     // now the top pipe sprite
     topPipe = new Sprite(400, midY - gap / 2 -200, 52, 320, 'static');
     topPipe.img = pipe;
+    topPipe.rotation = 180;
 
     pipeGroup.add(bottomPipe);
     pipeGroup.layer = 0; // go behind other sprites but on top of background image
