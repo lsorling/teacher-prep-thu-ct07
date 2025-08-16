cb6b63f20820d8c90cdccf5f20f9760fc1f7550a
lsorling <lsorling@yahoo.com.sg>
2025-08-16 13:27:02 +0000

    Aug 16, 2025, 1:27 PM

--- flappybird/playground/playground.js
+++ flappybird/playground/playground.js
@@ -9,9 +9,12 @@ let flapMidImg, flapUpImg, flapDownImg;
 // preload game assets like media and images
 function preload() {
     // bird image, background image, and the floor
-    flapMidImg = loadImage('assets/yellowbird-midflap.png');
     bg = loadImage('assets/background-day.png');
     base = loadImage('assets/base.png');
+
+    flapMidImg = loadImage('assets/yellowbird-midflap.png');
+    flapMidImg = loadImage('assets/yellowbird-midflap.png');
+    flapMidImg = loadImage('assets/yellowbird-midflap.png');
 }
 
 // run once like the "when green flag clicked"
