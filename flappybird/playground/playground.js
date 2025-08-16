0e53bb720ee921f8c4bc11c9ed6635066076040b
lsorling <lsorling@yahoo.com.sg>
2025-08-16 13:35:37 +0000

    Aug 16, 2025, 1:35 PM

--- flappybird/playground/playground.js
+++ flappybird/playground/playground.js
@@ -105,7 +105,8 @@ function spawnPipePair() {
     let gap = 50;
     let midY = height/2;
 
-    bottomPipe = new Sprite(400, midY, 52, 320, 'static');
+    bottomPipe = new Sprite(400, midY-200, 52, 320, 'static');
     bottomPipe.img = pipeImg;
-    pipeGroup.add(bo)
+    pipeGroup.add(bottomPipe);
+    pipeGroup.layer = 0;
 }
