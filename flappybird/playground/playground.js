f0187f5c80df84dcf2fad1061a88f127433faa65
lsorling <lsorling@yahoo.com.sg>
2025-09-05 03:14:52 +0000

    Sep 5, 2025, 3:14 AM

--- flappybird/playground/playground.js
+++ flappybird/playground/playground.js
@@ -179,7 +179,6 @@ function draw() {
             gameoverLabel = new Sprite(width/2, height/2, 192, 42);
             gameoverLabel.img = gameoverImg;
             gameoverLabel.layer = 100; // come to front most layer
-            gameoverLabel.rotation = 0;
             gameoverLabel.x = camera.x;
 
             dieSound.play();
