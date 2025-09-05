0a4fc66fede46f11e894efe887d1b333726e529b
lsorling <lsorling@yahoo.com.sg>
2025-09-05 03:04:32 +0000

    Sep 5, 2025, 3:04 AM

--- flappybird/playground/bluescreen.js
+++ flappybird/playground/bluescreen.js
@@ -36,14 +36,13 @@ function draw() {
         score--;
     }
     score = constrain(score,0,9);
-    scoreSprite.img = digitImgs[score];
     
     textSize(16);
     text("score: " + score, 20, 50);
 }
 
 function displayScore() {
-
+    scoreSprite.img = digitImgs[score];
 }
 
 /*
