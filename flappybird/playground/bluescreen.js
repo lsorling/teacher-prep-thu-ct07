23b889f9045b672c76efc16bee825f78a9eaeceb
lsorling <lsorling@yahoo.com.sg>
2025-09-05 03:11:23 +0000

    Sep 5, 2025, 3:11 AM

--- flappybird/playground/bluescreen.js
+++ flappybird/playground/bluescreen.js
@@ -48,7 +48,7 @@ function displayScore() {
     let scoreString = str(score);
     let scoreArray = scoreString.split("");
 
-    let offset = 0;
+    //let offset = 0;
     let offset = 0 - (scoreArray.length-1)*25/2;
     let middle = width/2;
 
