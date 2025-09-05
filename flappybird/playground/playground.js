a1e533893c23f6f2a5509a6e61a072673b4b78d6
lsorling <lsorling@yahoo.com.sg>
2025-09-05 03:18:11 +0000

    Sep 5, 2025, 3:18 AM

--- flappybird/playground/playground.js
+++ flappybird/playground/playground.js
@@ -253,7 +253,7 @@ function displayScore() {
 
     //let offset = 0;
     let offset = 0 - (scoreArray.length-1)*25/2;
-    let middle = width/2;
+    let middle = camera.x;
 
     text("array: "+ scoreArray, 10, 100);
     for (let one of scoreArray) {
